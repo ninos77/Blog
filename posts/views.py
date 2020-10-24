80faad265ce7a934c39a132a927fa7c69163f40b
ninos77 <ninos_zaiya@hotmail.com>
2020-10-25 00:40:17 +0200

    make detail page

--- posts/views.py
+++ posts/views.py
@@ -3,8 +3,17 @@ from .models import Post
 
 # Create your views here.
 
+
 def post(request):
   context = {
-    'posts' : Post.objects.all()
+    'posts': Post.objects.all()
   }
-  return render(request, "posts/posts.html",context)
+  return render(request, "posts/posts.html", context)
+
+
+ def detail(request, id):
+   post = Post.objects.get(id=id)
+     context = {
+    'post' : post
+  }
+  return render(request, "posts/detail.html",context)
